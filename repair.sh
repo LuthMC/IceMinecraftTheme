031392a0d2c8e36defacc15a730c3e1bbbcae326
LuthMC <luthfi366822@gmail.com>
2024-10-20 00:05:50 +0700

    Pterodactyl V1.11.5

--- repair.sh
+++ repair.sh
@@ -10,7 +10,7 @@ repairPanel(){
 
     rm -r /var/www/pterodactyl/resources
 
-    curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+    curl -L https://github.com/LuthMC/panel/releases/latest/download/panel.tar.gz | tar -xzv
 
     chmod -R 755 storage/* bootstrap/cache
 
